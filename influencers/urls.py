89720b7b1c13383b359b848ffffce89f3a487d56
Muhammad Haseeb Malik <63384019+Haseeb420@users.noreply.github.com>
2021-12-18 23:06:40 +0500

    malik

--- influencers/urls.py
+++ influencers/urls.py
@@ -19,15 +19,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 # from django.conf.urls import url
-from django.conf.urls import url
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('email_verification.urls')),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
-        (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
+    #url(r'^media/(?P<path>.*)$', serve,
+     #   {'document_root':       settings.MEDIA_ROOT}),
+    #url(r'^static/(?P<path>.*)$', serve,
+     #   {'document_root': settings.STATIC_ROOT}),
+      ##  (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
         
 ]
